bcccfbb1ef6cf9e750cfb2421538d2b625a05eab
Тетяна Жульєва <128372881+TanyaZhulieva@users.noreply.github.com>
2024-05-28 20:20:12 +0200

    basic

--- src/basic/7.ts
+++ src/basic/7.ts
@@ -1,29 +1,34 @@
-type OpenOrClose = 'open'|'close'
+type OpenOrClose = "open" | "close";
+
+type Details = {
+  createAt: Date;
+  updateAt: Date;
+};
 
 type Page = {
-    title: string,
-    likes: number,
-    accounts: string[],
-    status: OpenOrClose,
-    details?: {}
-}
+  title: string;
+  likes: number;
+  accounts: string[];
+  status: OpenOrClose;
+  details?: Details;
+};
 
+let page1: Page = {
+  title: "The awesome page",
+  likes: 100,
+  accounts: ["Max", "Anton", "Nikita"],
+  status: "open",
+  details: {
+    createAt: new Date("2021-01-01"),
+    updateAt: new Date("2021-05-01"),
+  },
+};
 
+let page2: Page = {
+  title: "Python or Js",
+  likes: 5,
+  accounts: ["Alex"],
+  status: "close",
+};
 
-const page1 = {
-    title: 'The awesome page',
-    likes: 100,
-    accounts: ['Max', 'Anton', 'Nikita'],
-    status: 'open',
-    details: {
-      createAt: new Date('2021-01-01'),
-      updateAt: new Date('2021-05-01'),
-    }
-  }
-  
-  const page2 = {
-    title: 'Python or Js',
-    likes: 5,
-    accounts: ['Alex'],
-    status: 'close',
-  }
+export {};
--- src/main.ts
+++ src/main.ts
@@ -6,3 +6,6 @@ import "./basic/3";
 import "./basic/4";
 import "./basic/5";
 import "./basic/6";
+import "./basic/7";
+
+import "./generics/1"
